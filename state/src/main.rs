5efd28ecb07a70bf53aee00abe815cbdbf794d6f
David Montiel <davidmontiel549@gmail.com>
2026-04-12 21:11:12 -0700

    edit file structure

---
+++ state/src/main.rs
@@ -0,0 +1,10 @@
+use opencv::{
+    Result,
+    core::{KeyPoint, Mat, Scalar, Vector},
+    features2d::{self, *},
+    highgui, imgcodecs,
+    prelude::*,
+};
+
+pub mod snap_shot;
+fn main() -> Result<()> {}
